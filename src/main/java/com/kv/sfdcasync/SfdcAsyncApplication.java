87f963f709dfa40cf4f4fbacdd17f7552061ca2b
arupsarkar <sarkar.arup@gmail.com>
2021-05-12 16:56:46 -0400

    Added db connection

--- src/main/java/com/kv/sfdcasync/SfdcAsyncApplication.java
+++ src/main/java/com/kv/sfdcasync/SfdcAsyncApplication.java
@@ -1,5 +1,12 @@
 package com.kv.sfdcasync;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,11 +17,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @EnableAutoConfiguration
 @SpringBootApplication
 public class SfdcAsyncApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(SfdcAsyncApplication.class);
+
+	@Value("${spring.datasource.url")
+	private String dbUrl;
+
 
 	@GetMapping("/home")
-	public String home() {
-		return "home";
+	public String home(Map<String, Object> model) {
+		String connSchema = "";
+		try{
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection(dbUrl);
+			connSchema = conn.getSchema();
+			LOG.debug("db schema ", connSchema);
+			model.put("schema", "Success - " + connSchema);
+			return "home";
+		}catch (Exception ex) {
+			model.put("schema", "Error - " + connSchema);
+			return "home";
+		}
 	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SfdcAsyncApplication.class, args);
